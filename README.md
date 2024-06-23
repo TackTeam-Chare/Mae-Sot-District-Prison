0bf025eede087de7bbf1aaa04bc09d1bf68502e2
TackTeam-Chare <chare.auppachitwan@gmail.com>
2024-06-23 22:41:13 +0700

    add animation

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 "# -" 
 "# Mae-Sot-District-Prison" 
+
+# วิธีรัน
+php -S localhost:8000 -t path/ของโปรเจค
--- index.php
+++ index.php
@@ -391,36 +391,7 @@
             </h1>
             <hr style="height: 5px;" color="aliceblue">
         </article>
-        <div style="background-color: rgb(175, 61, 61); height: 90px;width: 1400px;;">
-            <div class="sd">
-                <article>
-                    <figure>
-                        <a href="http://www.correct.go.th/infosaraban63/index.html">
-                            <img src="img/book3.png">
-                        </a>
-                        >
-                        <a href="https://www.etda.or.th/th/">
-                            <img src="img/สอท2.png">
-                        </a>
-                        <a href="http://www.correct.go.th/rt103/uploadsystem/">
-                            <img src="img/rt103.png">
-                        </a>
-                        <a href="http://www.correct.go.th/lawcorrects/">
-                            <img src="img/laws.png">
-                        </a>
-                        <a href="http://www.correct.go.th/stathomepage/">
-                            <img src="img/bf-26-1.jpg">
-                        </a>
-                        <a href="https://www.admincourt.go.th/admincourt/site/09illustration.html">
-                            <img src="img/bf-08.jpg">
-                        </a>
-                        <a href="https://drive.google.com/drive/folders/1qPU1XxpNIc5XXYseESVGmfyUU6jCutix?usp=sharin">
-                            <img src="img/bf-05.png">
-                        </a>
-                    </figure>
-                </article>
-            </div>
-        </div>
+     <?php include('./layout/animation.php'); ?>
   <?php include('./layout/footer.php'); ?>
     </body>
 </html>
