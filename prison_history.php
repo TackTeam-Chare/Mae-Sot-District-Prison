c2d4810d033e109e3fe29ace3d145d4a7a45d727
TackTeam-Chare <chare.auppachitwan@gmail.com>
2024-06-24 17:57:03 +0700

    add .

--- prison_history.php
+++ prison_history.php
@@ -1,17 +1,42 @@
 <!doctype html>
 <html>
-    <head>
-        <title>ประวัติเรือนจำ</title>
-        <link rel="icon" type="image/x-icon" href="img/spd_20150704164759_b.png">
-        <style>
-          body {
-            background-image: url('img/รูปเรือนจำ.jpg');
-            background-repeat: no-repeat;
-            background-attachment: fixed;  
-            background-size: cover;
-            font-family: sans-serif;
-          }
-          </style>
+
+<head>
+  <title>ประวัติเรือนจำ</title>
+  <link rel="icon" type="image/x-icon" href="img/spd_20150704164759_b.png">
+  <style>
+    body {
+      background-image: url('img/รูปเรือนจำ.jpg');
+      background-repeat: no-repeat;
+      background-attachment: fixed;
+      background-size: cover;
+    }
+
+    .content {
+      background-color: rgba(0, 0, 0, 0.7);
+      padding: 20px;
+      border-radius: 10px;
+      margin-top: 20px;
+      margin-bottom: 20px;
+      /* เพิ่มการเว้นระยะด้านล่าง */
+    }
+
+    .content h2 {
+      color: #fff;
+      margin-bottom: 20px;
+    }
+
+    .content h4 {
+      color: #fff;
+      margin-bottom: 20px;
+    }
+
+    .mission-title {
+      color: rgb(0, 0, 0);
+      margin-top: 20px;
+      margin-bottom: 20px;
+    }
+  </style>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta charset="utf-8">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
@@ -19,30 +44,32 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
-    <?php include('./layout/navbar.php'); ?>
-        <h1><span style="color: rgb(0, 0, 0);">ประวัติเรือนจำ</span></h1>
-          <br>
-          <img class="city x"  src="img/phpiWvmR0.png">
-          <br>
-          <br>
-          <article>
-            <h4><span style="color:rgb(0, 0, 0)">   เรือนจำอำเภอแม่สอด เดิมตั้งอยู่หลังสถานีตำรวจภูธรอำเภอแม่สอด<br>
-            สร้างเมื่อ เมื่อปี พ.ศ. ๒๔๖๙ มีพื้นที่ทั้งหมด ๕ ไร่เศษ มีกำแพงเรือนจำเป็นไม้<br>
-              มีป้อมรักษาการณ์ บนสันกำแพง ๒ ป้อม เรือนนอนชายเป็นไม้สัก จำนวน ๑ หลัง<br>
-              แดนผู้ต้องขังหญิงใช้เรือนพยาบาลเป็นเรือนนอน จำนวน ๑ หลัง มีนายอำเภอ<br>
-              เป็นผู้บัญชาการเรือนจำโดย ตำแหน่ง ต่อมานายธีรศักดิ์ วงรักษ์ ซึ่งเป็นพัสดีเรือนจำ<br>
-              อำเภอแม่สอด และปฏิบัติหน้าที่ผู้บัญชาการเรือนจำแทนนายอำเภอเรือนจำอำเภอ<br>
-              แม่สอด ได้พิจารณาเห็นว่าเรือนจำอำเภอแม่สอด(เดิม)มีสถานที่คับแคบไม่เพียงพอ<br>
-              แก่จำนวนผู้ต้องขังในอนาคต จึงได้พิจารณาสถานที่สำหรับก่อสร้างเรือนจำแห่งใหม่<br>
-              เพื่อให้มีพื้นที่เพียงพอรองรับกับจำนวนผู้ต้องขังที่มีแนวโน้มที่จะเพิ่มขึ้นต่อมา
-              ในปี <br>พ.ศ. ๒๕๑๗เรือนจำใหม่ ณ เรือนจำชั่วคราววัดดอนไชย ในเนื้อที่ ๒๑ไร่ ๓ งาน<br>
-               ๒๐ตรางวาเปิดทำการ ในปี พ.ศ. ๒๕๑๙ และมีผู้บัญชาการเรือนจำเฉพาะตำแหน่งเมื่อปี <br>
-               พ.ศ.๒๕๒๐เป็นต้นมาจนถึงปัจุบัน</span></h4>
-               <br>
-          </article>
-        </section>
-        <?php include('./layout/footer.php'); ?>        
-    </body>
+</head>
+
+<body>
+  <?php include('./layout/navbar.php'); ?>
+  <h1 class="mission-title"><span>ประวัติเรือนจำ</span></h1>
+  <br>
+  <img class="city x" src="img/phpiWvmR0.png">
+  <br>
+  <br>
+  <article class="content">
+    <h4><span> เรือนจำอำเภอแม่สอด เดิมตั้งอยู่หลังสถานีตำรวจภูธรอำเภอแม่สอด<br>
+        สร้างเมื่อ เมื่อปี พ.ศ. ๒๔๖๙ มีพื้นที่ทั้งหมด ๕ ไร่เศษ มีกำแพงเรือนจำเป็นไม้<br>
+        มีป้อมรักษาการณ์ บนสันกำแพง ๒ ป้อม เรือนนอนชายเป็นไม้สัก จำนวน ๑ หลัง<br>
+        แดนผู้ต้องขังหญิงใช้เรือนพยาบาลเป็นเรือนนอน จำนวน ๑ หลัง มีนายอำเภอ<br>
+        เป็นผู้บัญชาการเรือนจำโดย ตำแหน่ง ต่อมานายธีรศักดิ์ วงรักษ์ ซึ่งเป็นพัสดีเรือนจำ<br>
+        อำเภอแม่สอด และปฏิบัติหน้าที่ผู้บัญชาการเรือนจำแทนนายอำเภอเรือนจำอำเภอ<br>
+        แม่สอด ได้พิจารณาเห็นว่าเรือนจำอำเภอแม่สอด(เดิม)มีสถานที่คับแคบไม่เพียงพอ<br>
+        แก่จำนวนผู้ต้องขังในอนาคต จึงได้พิจารณาสถานที่สำหรับก่อสร้างเรือนจำแห่งใหม่<br>
+        เพื่อให้มีพื้นที่เพียงพอรองรับกับจำนวนผู้ต้องขังที่มีแนวโน้มที่จะเพิ่มขึ้นต่อมา
+        ในปี <br>พ.ศ. ๒๕๑๗เรือนจำใหม่ ณ เรือนจำชั่วคราววัดดอนไชย ในเนื้อที่ ๒๑ไร่ ๓ งาน<br>
+        ๒๐ตรางวาเปิดทำการ ในปี พ.ศ. ๒๕๑๙ และมีผู้บัญชาการเรือนจำเฉพาะตำแหน่งเมื่อปี <br>
+        พ.ศ.๒๕๒๐เป็นต้นมาจนถึงปัจุบัน</span></h4>
+    <br>
+  </article>
+  </section>
+  <?php include('./layout/footer.php'); ?>
+</body>
+
 </html>
