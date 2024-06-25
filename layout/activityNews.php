6c0501f718f3c497200192a9ac24b77b3cf60d8f
TackTeam-Chare <chare.auppachitwan@gmail.com>
2024-06-25 15:23:07 +0700

    add .

--- layout/activityNews.php
+++ layout/activityNews.php
@@ -7,54 +7,38 @@
     <title>ข่าวกิจกรรม</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/css/bootstrap.min.css">
     <style>
-        .card {
+        #card {
             transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
             height: 100%;
             display: flex;
             flex-direction: column;
         }
 
-        .card:hover {
+        #card:hover {
             transform: translateY(-5px);
             box-shadow: 0px 5px 15px rgba(0, 0, 0, 0.3);
         }
 
-        .card-body {
+        #card-body {
             padding: 1rem;
             display: flex;
             flex-direction: column;
             flex-grow: 1;
         }
 
-        .card-title {
+        #card-title {
             font-size: 1.25rem;
         }
 
-        .card-subtitle {
+        #card-subtitle {
             font-size: 0.875rem;
-            color: white; 
+            color: white;
             font-weight: bold;
         }
 
-        .card-text {
+        #card-text {
             font-size: 1rem;
             flex-grow: 1;
-        }
-        .row-cols-6 {
-            gap: 20px;
-
-        }
-        .container {
-            margin-left: auto;
-
-            margin-right: auto;
-
-            max-width: 100%;
-        }
-        .row {
-            display: flex;
-            justify-content: center;
-            align-items: center;
         }
     </style>
 </head>
@@ -63,47 +47,71 @@
     <article class="container">
         <h1 class="top23 text-center" style="color: rgb(255, 255, 255);">ข่าวกิจกรรม</h1>
         <br>
-        <div class="row row-cols-1 row-cols-md-6 g-3 justify-content-center align-items-center">
-            <div class="col mb-3 aos-init aos-animate">
-                <a class="card text-reset text-decoration-none rounded-15" href="new.php" data-pjax="" data-pjax-state="">
+        <div class="row g-3">
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+                <a id="card" class="card text-reset text-decoration-none rounded-15" href="new.php" data-pjax="" data-pjax-state="">
                     <img src="img/พ่นยาคลอรีนและแอลกอฮอล์ 9 มีนาคม 2565.jpg" class="card-img-top img-autofit rounded-15" alt="news" style="height:200px;">
-                    <div class="card-body">
-                        <h5 class="card-title badge bg-primary text-wrap">พ่นยาคลอรีนและแอลกอฮอล์</h5>
-                        <h6 class="card-subtitle mb-2 ">9 มี.ค. 2565</h6>
-                        <p class="card-text ">ข้อมูลการพ่นยาคลอรีนและแอลกอฮอล์</p>
+                    <div id="card-body" class="card-body">
+                        <h5 id="card-title" class="card-title badge bg-primary text-wrap">พ่นยาคลอรีนและแอลกอฮอล์</h5>
+                        <h6 id="card-subtitle" class="card-subtitle mb-2 ">9 มี.ค. 2565</h6>
+                        <p id="card-text" class="card-text ">ข้อมูลการพ่นยาคลอรีนและแอลกอฮอล์</p>
                     </div>
                 </a>
             </div>
-            <div class="col mb-3 aos-init aos-animate">
-                <a class="card text-reset text-decoration-none rounded-15" href="new.php" data-pjax="" data-pjax-state="">
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+                <a id="card" class="card text-reset text-decoration-none rounded-15" href="new.php" data-pjax="" data-pjax-state="">
                     <img src="img/พ่นยาคลอรีนและแอลกอฮอล์ 9 มีนาคม 2565.jpg" class="card-img-top img-autofit rounded-15" alt="news" style="height:200px;">
-                    <div class="card-body">
-                        <h5 class="card-title badge bg-primary text-wrap ">พ่นยาคลอรีนและแอลกอฮอล์</h5>
-                        <h6 class="card-subtitle mb-2 ">9 มี.ค. 2565</h6>
-                        <p class="card-text ">ข้อมูลการพ่นยาคลอรีนและแอลกอฮอล์</p>
+                    <div id="card-body" class="card-body">
+                        <h5 id="card-title" class="card-title badge bg-primary text-wrap">พ่นยาคลอรีนและแอลกอฮอล์</h5>
+                        <h6 id="card-subtitle" class="card-subtitle mb-2 ">9 มี.ค. 2565</h6>
+                        <p id="card-text" class="card-text ">ข้อมูลการพ่นยาคลอรีนและแอลกอฮอล์</p>
                     </div>
                 </a>
             </div>
-            <div class="col mb-3 aos-init">
-                <a class="card text-reset text-decoration-none rounded-15" href="new.php" data-pjax="" data-pjax-state="">
-                    <img src="img/พ่นยาคลอรีนและแอลกอฮอล์ 6 มีนาคม 2565.jpg" class="card-img-top img-autofit rounded-15" alt="news" style="height:200px;">
-                    <div class="card-body">
-                        <h5 class="card-title badge bg-primary text-wrap">พ่นยาคลอรีนและแอลกอฮอล์</h5>
-                        <h6 class="card-subtitle mb-2 ">6 มี.ค. 2565</h6>
-                        <p class="card-text ">ข้อมูลการพ่นยาคลอรีนและแอลกอฮอล์</p>
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+                <a id="card" class="card text-reset text-decoration-none rounded-15" href="new.php" data-pjax="" data-pjax-state="">
+                    <img src="img/พ่นยาคลอรีนและแอลกอฮอล์ 9 มีนาคม 2565.jpg" class="card-img-top img-autofit rounded-15" alt="news" style="height:200px;">
+                    <div id="card-body" class="card-body">
+                        <h5 id="card-title" class="card-title badge bg-primary text-wrap">พ่นยาคลอรีนและแอลกอฮอล์</h5>
+                        <h6 id="card-subtitle" class="card-subtitle mb-2 ">9 มี.ค. 2565</h6>
+                        <p id="card-text" class="card-text ">ข้อมูลการพ่นยาคลอรีนและแอลกอฮอล์</p>
                     </div>
                 </a>
             </div>
-            <div class="col mb-3 aos-init">
-                <a class="card text-reset text-decoration-none rounded-15" href="new.php">
-                    <img src="img/พัฒนาคุณภาพ.jpg" class="card-img-top img-autofit rounded-15" alt="news" style="height:200px;">
-                    <div class="card-body">
-                        <h5 class="card-title badge bg-primary text-wrap">การยกระดับฝึกอาชีพ การพัฒนาการตลาดให้กับสินค้า และพัฒนาคุณภาพชีวิตผู้พ้นโทษ</h5>
-                        <h6 class="card-subtitle mb-2 ">7 ก.พ. 2565</h6>
-                        <p class="card-text">โครงการส่งเสริมทักษะอาชีพผู้ต้องขังด้านการเลี้ยงสุนัขเพื่อพัฒนาพฤตินิสัยผู้ต้องขัง</p>
+    
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+                <a id="card" class="card text-reset text-decoration-none rounded-15" href="new.php" data-pjax="" data-pjax-state="">
+                    <img src="img/พ่นยาคลอรีนและแอลกอฮอล์ 9 มีนาคม 2565.jpg" class="card-img-top img-autofit rounded-15" alt="news" style="height:200px;">
+                    <div id="card-body" class="card-body">
+                        <h5 id="card-title" class="card-title badge bg-primary text-wrap">พ่นยาคลอรีนและแอลกอฮอล์</h5>
+                        <h6 id="card-subtitle" class="card-subtitle mb-2 ">9 มี.ค. 2565</h6>
+                        <p id="card-text" class="card-text ">ข้อมูลการพ่นยาคลอรีนและแอลกอฮอล์</p>
                     </div>
                 </a>
             </div>
+    
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+                <a id="card" class="card text-reset text-decoration-none rounded-15" href="new.php" data-pjax="" data-pjax-state="">
+                    <img src="img/พ่นยาคลอรีนและแอลกอฮอล์ 9 มีนาคม 2565.jpg" class="card-img-top img-autofit rounded-15" alt="news" style="height:200px;">
+                    <div id="card-body" class="card-body">
+                        <h5 id="card-title" class="card-title badge bg-primary text-wrap">พ่นยาคลอรีนและแอลกอฮอล์</h5>
+                        <h6 id="card-subtitle" class="card-subtitle mb-2 ">9 มี.ค. 2565</h6>
+                        <p id="card-text" class="card-text ">ข้อมูลการพ่นยาคลอรีนและแอลกอฮอล์</p>
+                    </div>
+                </a>
+            </div>
+    
+            <div class="col-lg-4 col-md-6 col-sm-12 mb-3">
+                <a id="card" class="card text-reset text-decoration-none rounded-15" href="new.php" data-pjax="" data-pjax-state="">
+                    <img src="img/พ่นยาคลอรีนและแอลกอฮอล์ 9 มีนาคม 2565.jpg" class="card-img-top img-autofit rounded-15" alt="news" style="height:200px;">
+                    <div id="card-body" class="card-body">
+                        <h5 id="card-title" class="card-title badge bg-primary text-wrap">พ่นยาคลอรีนและแอลกอฮอล์</h5>
+                        <h6 id="card-subtitle" class="card-subtitle mb-2 ">9 มี.ค. 2565</h6>
+                        <p id="card-text" class="card-text ">ข้อมูลการพ่นยาคลอรีนและแอลกอฮอล์</p>
+                    </div>
+                </a>
+            </div>
+    
         </div>
     </article>
 </body>
