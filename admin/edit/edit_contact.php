adba16b75138b782958a77ec1548348078b8958e
TackTeam-Chare <chare.auppachitwan@gmail.com>
2024-06-29 17:28:00 +0700

    add .

--- admin/edit/edit_contact.php
+++ admin/edit/edit_contact.php
@@ -70,7 +70,7 @@
             </div>
         </form>
     </div>
-    <?php include_once('../layout/footer.php') ?>
+  
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
 </body>
